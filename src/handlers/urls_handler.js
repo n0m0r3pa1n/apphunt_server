92c2f3d8282a8c45cc58a0af66b591367dcbfaf6
Venelin Valkov <venelin@bapplz.com>
2015-02-19 15:32:27 +0200

    Get url from HuntMe

--- src/handlers/urls_handler.js
+++ src/handlers/urls_handler.js
@@ -3,7 +3,7 @@ var HuntMe = require('huntme-node')
 function* getShortLink(link) {
     var huntMe = new HuntMe("")
     var response = yield huntMe.createLink(link, "Android")
-    return response.link
+    return response.url
 }
 
 module.exports.getShortLink = getShortLink
