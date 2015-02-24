fbcf0c597fb53e3c3c00230cf35eb8eb68c39776
Venelin Valkov <venelin@bapplz.com>
2015-02-24 16:00:48 +0200

    Add HuntMe token

--- src/handlers/urls_handler.js
+++ src/handlers/urls_handler.js
@@ -1,7 +1,7 @@
 var HuntMe = require('huntme')
 
 function* getShortLink(link) {
-    var huntMe = new HuntMe("")
+    var huntMe = new HuntMe("54ec824e61fc8103004a436a")
     var response = yield huntMe.createLink(link, "Android")
     return response.url
 }
