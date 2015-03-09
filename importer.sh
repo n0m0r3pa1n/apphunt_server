4dae56f7f4e42525634b2a4d155f9ffb0cc5c046
Georgi Mirchev <gmirchev90@gmail.com>
2015-03-09 20:51:25 +0200

    Script to import apphunt db locally

---
+++ importer.sh
@@ -0,0 +1,10 @@
+#
+#
+# Script to import apphunt server db to localhost
+#
+echo "Importing DB to localhost"
+mongodump -h ds031531.mongolab.com:31531 -d heroku_app33343837 -u NaughtySpirit -p fmi123 -o ~/Desktop
+mongorestore -h localhost:27017 -d apphunt ~/Desktop/heroku_app33343837
+rm -r ~/Desktop/heroku_app33343837
+echo "Finished"
+exit 0
