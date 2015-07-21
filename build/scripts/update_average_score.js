cd8db87c2135df41a96046db3e45e1729a5a8d47
Georgi Mirchev <gmirchev90@gmail.com>
2015-07-21 16:22:47 +0300

    Average score script

---
+++ build/scripts/update_average_score.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var Mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/apphunt';
+// Dev DB URI
+//var dbURI = 'mongodb://NaSp:fmi123@ds031877.mongolab.com:31877/heroku_948fv92g'
+// Prod DB URI
+var dbURI = 'mongodb://NaughtySpirit:fmi123@ds031531.mongolab.com:31531/heroku_app33343837';
+
+Mongoose.connect(dbURI);
+
+var Co = require('co');
+var App = require('../models').App;
+var DevsHunter = require('../handlers/utils/devs_hunter_handler');
+
+Co(function* () {
+    var apps = yield App.find({ $or: [{ averageScore: 0 }, { averageScore: undefined }] }).exec();
+    var size = apps.length;
+    var i = 0;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = apps[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var app = _step.value;
+
+            i++;
+            var parsedApp = yield DevsHunter.getAndroidApp(app['package']);
+            if (parsedApp == null) {
+                continue;
+            }
+            console.log('Update ' + i + ' of ' + size);
+            app.averageScore = parsedApp.score.total;
+            yield app.save();
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator['return']) {
+                _iterator['return']();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+});
