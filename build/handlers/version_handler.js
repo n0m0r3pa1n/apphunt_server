4bb3dd6125af7c6047f27427d6a4d5876670fa98
Georgi Mirchev <gmirchev90@gmail.com>
2015-08-04 16:38:17 +0300

    Get version from app schema

--- build/handlers/version_handler.js
+++ build/handlers/version_handler.js
@@ -4,9 +4,13 @@ Object.defineProperty(exports, '__esModule', {
     value: true
 });
 exports.getLatestVersionCode = getLatestVersionCode;
-
-var _configConfigJs = require('../config/config.js');
+var AppVersion = require('../models').AppVersion;
+var LATEST_APP_VERSION = require('../config/config').LATEST_APP_VERSION;
 
 function* getLatestVersionCode() {
-    return { versionCode: _configConfigJs.LATEST_APP_VERSION };
+    var appVersion = yield AppVersion.findOne({});
+    if (appVersion == null) {
+        appVersion = LATEST_APP_VERSION;
+    }
+    return { versionCode: appVersion };
 }
